ed2e6e1d4f6e105b952dbc7197cc23dc70e83775
jianghu85 <1935469199@qq.com>
2017-04-14 13:37:33 +0800

    孟妍第一次提交的detail和chat初稿

--- snail_vue/src/components/Chat.vue
+++ snail_vue/src/components/Chat.vue
@@ -1,6 +1,23 @@
 <template>
   <div>
-    <h1>{{msg}}</h1>
+    <div id="chat_header">
+      <a href="#/index" class="chat_a1"><i class="iconfont">&#xe62e;</i></a>
+      <a class="chat_a2"><span class="chat_span1">{{msg}}</span><span class="chat_span2">聊天</span></a>
+      <a class="chat_a3"><i class="iconfont">&#xe686;</i></a>
+    </div>
+    <div id="chat_content">
+      <div class="chat_box1">
+        <img src="../resource/img/chat/1.jpg" />
+        <span class="chat_span3">刘西城</span>
+        <span class="chat_span4">服务于苏州</span>
+      </div>
+      <p class="chat_p1">
+        【尊享魅力设计，百变品质生活！】设计是一种追求完美的生活态度，设计是一种追求完美的生活态度，设计是一种追求完美的生活态。，设计是一种追求完美的生活态度，设计是一种追求完美的生活态度，设计是一种追求完美的生活态度。
+      </p>
+      <div class="chat_box2">
+        <img src="../resource/img/detail/1.jpg" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,7 +26,7 @@ export default {
   name: 'chat',
   data () {
     return {
-      msg: '这个是 chat 页面'
+      msg: '资料'
     }
   }
 }
@@ -17,8 +34,68 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: 10px;
+#chat_header{
+  height:1rem;
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 0.01rem solid #eee;
+  line-height: 1rem;
+  padding:0 0.4rem;
 }
+#chat_header a{
+  text-decoration: none;
+  font-size: 0.28rem;
+  color:#000;
+  font-size: 0.4rem;
+}
+.chat_span1{
+  margin-right: 0.1rem;
+}
+.chat_span2{
+  margin-left: 0.1rem;
+}
+#chat_header a.chat_a3 i{
+  font-size: 0.6rem;
+}
+.chat_box1{
+  height:5.8rem;
+  width:100%;
+  position: relative;
+}
+.chat_box1 img{
+  height:100%;
+  width:100%;
+}
+.chat_span3{
+  position: absolute;
+  top:4.7rem;
+  left: 0.29rem;
+  color: #fff;
+  font-size: 0.35rem;
+}
+.chat_span4{
+  position: absolute;
+  top:5.2rem;
+  left: 0.29rem;
+  color: #fff;
+  font-size: 0.2rem;
+}
+.chat_p1{
+  margin: 0.25rem;
+  font-size: 0.25rem;
+  line-height: 1.5;
+  font-weight: bolder;
+}
+.chat_box2{
+  height:6.8rem;
+  width: 100%;
+  background:#f6f6f6;
+  padding:0.4rem 0;
+}
+.chat_box2 img{
+  width:100%;
+  height: 100%;
+}
+
 
 </style>
--- snail_vue/src/components/Detail.vue
+++ snail_vue/src/components/Detail.vue
@@ -1,6 +1,47 @@
 <template>
   <div>
-    <h1>{{msg}}</h1>
+    <div id="detail_header">
+      <a href="#/index" class="detail_a1 l"><i class="iconfont">&#xe62e;</i></a>
+      <a class="r detail_a2"><i class="iconfont">&#xe656;</i>{{msg}}</a>
+    </div>
+    <div id="detail_content">
+      <div class="detail_content_top">
+        <h1 class="detail_content_h1">自由国度</h1>
+        <em class="detail_content_p1">94㎡，四室一厅，北欧，宁波</em>
+        <p class="detail_content_p2">
+          <i class="iconfont">&#xe6e3;</i>
+          <span class="detail_content_span1">赵四</span>
+        </p>
+      </div>
+      <ul>
+        <li>
+          <div class="detail_content_img">
+            <img src="../resource/img/detail/6.jpg">
+            <span class="detail_content_span2"><i class="iconfont">&#xe614;</i></span>
+          </div>
+          <div class="detail_content_box"> 
+            <em class="detail_content_b1">餐厅</em>
+            <p class="detail_content_p3">北欧风格的餐厅哈恩美丽啦，北欧风格的餐厅哈恩美丽啦。北欧风格的餐厅哈恩美丽啦，北欧风格的餐厅哈恩美丽啦。北欧风格的餐厅哈恩美丽啦。</p>
+          </div>
+        </li>
+      </ul>
+      <div class="detail_content_bottom">
+        <p class="detail_content_p4">74.9K 浏览 | 1.3K 收藏</p>
+        <div class="detail_content_box1">
+          <div class="detail_content_div1 l"><i class="iconfont">&#xe612;</i></div>
+          <div class="detail_content_div2 l">
+            <p class="detail_content_p6"><b class="detail_content_b2 l">赵四</b><span class="detail_content_em l">长沙</span></p>
+            <p class="detail_content_p5">设计触及灵魂。服务感动心灵</p>
+          </div>
+        </div>
+        <p class="detail_content_p7">
+          室内设计：50-200￥/㎡ >
+        </p>
+      </div>
+    </div>
+
+   
+   
   </div>
 </template>
 
@@ -9,7 +50,7 @@ export default {
   name: 'detail',
   data () {
     return {
-      msg: '这个是 detail 页面'
+      msg: '作品信息'
     }
   }
 }
@@ -17,8 +58,159 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: 10px;
+#detail_header{
+  height:1rem;
+  width: 100%;
+  padding-bottom: 0.15rem;
+  border-bottom: 0.01rem solid #eee;
+  position: relative;
+  font-family: "微软雅黑";
+  top: 0;
+  line-height: 1rem;
 }
+.detail_a1{
+  text-decoration: none;
+  color:#000;margin-left: 0.4rem;
+  font-size: 0.4rem;
+}
+.detail_a2{
+  font-size: 0.25rem;
+  margin-right:0.4rem;
+}
+.detail_a2 .iconfont{
+  margin-right: 0.1rem;
+  font-size: 0.2rem;
+}
+#detail_content{
+  overflow: hidden;
+  margin-bottom: 1rem;
+}
+.detail_content_top{
+  margin-left: 0.5rem;
+  margin-top: 0.8rem;
+}
+.detail_content_h1{
+  font-size: 0.4rem;
+}
+.detail_content_p1{
+  height:0.5rem;
+  color:#ddd;
+  background-color: #fefefe;
+  padding:0.15rem;
+  font-size: 0.2rem;
+  font-style:normal;
+  border-radius: 0.25rem;
+}
+.detail_content_p2 .iconfont{
 
+}
+#detail_content ul li{
+}
+.detail_content_box{
+  margin-left: 0.5rem;
+  margin-right: 0.5rem;
+}
+.detail_content_span1{
+  font-size: 0.35rem;
+}
+.detail_content_img{
+  width:100%;
+  height: 5rem;
+  position: relative;
+}
+.detail_content_img img{
+  height:100%;
+  width: 100%;
+}
+.detail_content_span2{
+  position: absolute;
+  right:0.5rem;
+  bottom:0.3rem;
+  height:0.6rem;
+  width:0.6rem;
+  background-color: #000;
+  opacity: 0.5;
+  border-radius: 50%;
+}
+.detail_content_span2 .iconfont{
+  color:#fff;
+  position: absolute;
+  left:0.15rem;
+  top:0.14rem;
+}
+.detail_content_b1{
+  font-size: 0.2rem;
+  height:0.3rem;
+  border-radius:0.2rem;
+  border:0.01rem solid #3e3e3e; 
+  font-style:normal;
+  padding:0.05rem 0.2rem;
+}
+.detail_content_p3{
+  font-size: 0.2rem;
+  font-weight: bloder;
+}
+.detail_content_bottom{
+  margin-left: 0.5rem;
+  margin-right: 0.5rem;
+  margin-top: 1rem;
+}
+.detail_content_box1{
+  margin-top:0.6rem;
+  height:2rem;
+  overflow: hidden;
+}
+.detail_content_p4{
+  font-size: 0.16rem;
+  color: #cbcbcb;
+  padding-bottom: 0.30rem;
+  border-bottom: 0.01rem solid #efefef;
+}
+.detail_content_div1{
+  height:1.5rem;
+}
+.detail_content_div2{
+  height:1.5rem;
+  margin-left: 0.26rem;
+}
+.detail_content_div1 .iconfont{
+  font-size: 1rem;
+}
+.detail_content_b2{
+  font-size: 0.3rem;
+  margin-right:0.2rem;
+}
+.detail_content_em{
+  font-size: 0.15rem;
+  height:0.5rem;
+  color:#ddd;
+  background-color: #fefefe;
+  padding:0.15rem;
+  font-size: 0.2rem;
+  font-style:normal;
+  border-radius: 0.25rem;
+  margin-top: -0.19rem;
+}
+.detail_content_p5{
+  font-size: 0.2rem;
+}
+.detail_content_p6{
+  height:0.8rem;
+  overflow: hidden;
+  line-height: 0.8rem;
+}
+.detail_content_p7{
+  text-align: center;
+  font-weight: bold;
+  height:0.7rem;
+  line-height: 0.7rem;
+  color:#0eb;
+  background-color: #fefefe;
+  padding:0.15rem;
+  font-size: 0.2rem;
+  font-style:normal;
+  border-radius: 0.25rem;
+  margin: 0 auto;
+  background-color: #e5ffed;
+}
 </style>
